0f1fc9307c0daddcfe78f15a3f8702ad11159259
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-11 23:51:46 -0300

    ♻️ refactor: abstrai lógica de busca com função utilitária `criarTarefa`

--- src/store/middlewares/categorias.ts
+++ src/store/middlewares/categorias.ts
@@ -1,54 +1,24 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit'
-import { createStandaloneToast } from '@chakra-ui/toast'
 import { adicionarTodasAsCategorias, carregarCategorias, carregarUmaCategoria } from '@/store/reducers/categorias'
 import categoriasService from '@/services/categorias'
-
-const { toast } = createStandaloneToast()
+import criarTarefa from './utils/criarTarefa'
 
 export const listener = createListenerMiddleware()
 
 listener.startListening({
     actionCreator: carregarCategorias,
     effect: async (action, { dispatch, fork, unsubscribe }) => {
-        toast({
-            title: 'Carregando',
-            description: 'Carregando categorias',
-            status: 'loading',
-            duration: 2000,
-            isClosable: true,
+        await criarTarefa({
+            fork,
+            dispatch,
+            action: adicionarTodasAsCategorias,
+            busca: categoriasService.buscar,
+            textoCarregando: 'Carregando categorias',
+            textoSucesso: 'Categorias carregadas com sucesso!',
+            textoErro: 'Erro na busca de categorias',
         })
 
-        const tarefa = fork(async (api) => {
-            await api.delay(1000)
-
-            return await categoriasService.buscar()
-        })
-
-        const resposta = await tarefa.result
-
-        if (resposta.status === 'ok') {
-            toast({
-                title: 'Sucesso!',
-                description: 'Categorias carregadas com sucesso!',
-                status: 'success',
-                duration: 2000,
-                isClosable: true,
-            })
-
-            dispatch(adicionarTodasAsCategorias(resposta.value))
-
-            unsubscribe()
-        }
-
-        if (resposta.status === 'rejected') {
-            toast({
-                title: 'Erro',
-                description: 'Erro na busca de categorias',
-                status: 'error',
-                duration: 2000,
-                isClosable: true,
-            })
-        }
+        unsubscribe()
     },
 })
 
