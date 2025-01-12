9bf8305f7abbea44ce924462d9c081ddec434c39
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-12 17:20:44 -0300

    ✨ feat: adiciona `listenerMiddleware` para facilitar o gerenciamento de listeners no Redux

---
+++ src/store/middlewares/listenerMiddleware.ts
@@ -0,0 +1,9 @@
+import { createListenerMiddleware } from '@reduxjs/toolkit'
+import { RootState, AppDispatch } from '@/store'
+
+export const listenerMiddleware = createListenerMiddleware()
+
+export const startAppListening = listenerMiddleware.startListening.withTypes<
+    RootState,
+    AppDispatch
+>()
